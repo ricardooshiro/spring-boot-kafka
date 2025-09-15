e87a2bddaf17e87072d9786f8762e5531024fa0e
ricardooshiro <ricardooshiro@yahoo.com.br>
2025-09-14 22:49:01 -0300

    Messaging Spring Boot Kafka

---
+++ src/main/java/com/ricardooshiro/messagingspringboot/MessagingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ricardooshiro.messagingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingSpringbootApplication.class, args);
+	}
+
+}
